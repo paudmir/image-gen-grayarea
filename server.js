b780c64ef8ccb47047e6e46eaf4348691348a84e
Paulina Duran <winterdamsel@gmail.com>
2025-11-12 21:15:01 -0800

    Switching port, see if that helps

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const fetch = require('node-fetch');
 
 const app = express();
-const PORT = 3000;
+const PORT = Number(process.env.PORT || 3000);
 
 // Middleware
 app.use(cors());
